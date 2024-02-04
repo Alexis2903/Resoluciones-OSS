eabb99860bd023e592841fc9b6f0448a1072b24b
Alexis2903 <steeven.encalada281@ist17dejulio.edu.ec>
2024-02-04 11:44:48 -0500

    Conexion BDD

---
+++ src/main/java/DIU/CONTROLADOR/MainBDD.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package DIU.CONTROLADOR;
+
+/**
+ *
+ * @author jefe
+ */
+public class MainBDD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ConexionBDD Conecion=new ConexionBDD();
+Conecion.conectar();
+
+    }
+    
+}
